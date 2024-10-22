c37f5aecf01efae9c56519e3ec22d7ca48084252
Luis-Fernando-Orta <156867799+Luis-Fernando-Orta@users.noreply.github.com>
2024-10-21 22:45:04 -0600

    Main de autenticacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'; 
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx';
 import './index.css';
 
 const rootElement = document.getElementById('root');
-
-
 const root = createRoot(rootElement); 
+
+
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+      domain='dev-umff0vrcgvopf8c5.us.auth0.com'
+      clientId='s6klbLwwDCMnvHJr4qThMgpL7ksryiw3'
+      redirectUri={window.location.origin}
+    >
     <App />
-  </React.StrictMode>
+    </Auth0Provider>
+  </React.StrictMode>,
+  
+  
 );
